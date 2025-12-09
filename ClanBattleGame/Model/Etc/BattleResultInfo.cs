aa5b70ca504fe3bc5c5e3faa49d3fd8afd308347
Deadlyss ㅤ <dimalimar28@gmail.com>
2025-12-10 01:31:47 +0200

    add game result window

---
+++ ClanBattleGame/Model/Etc/BattleResultInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace ClanBattleGame.Model.Etc
+{
+    public class BattleResultInfo
+    {
+        public bool PlayerWon { get; set; }
+        public int UnitsKilled { get; set; }
+        public int UnitsLost { get; set; }
+        public int Turns { get; set; }
+        public int MoneyEarned { get; set; }
+    }
+}
---
+++ ClanBattleGame/ViewModel/GameResultVM.cs
@@ -0,0 +1,37 @@
+﻿using ClanBattleGame.Core;
+using ClanBattleGame.Model.Etc;
+using ClanBattleGame.Service;
+using System.Windows;
+
+namespace ClanBattleGame.ViewModel
+{
+    public class GameResultVM : ObservableObject
+    {
+        private readonly NavigationStore _navStore;
+        private readonly BattleResultInfo _result;
+
+        public GameResultVM(NavigationStore navStore, BattleResultInfo result)
+        {
+            _navStore = navStore;
+            _result = result;
+
+            ReturnCommand = new RelayCommand(_ =>
+            {
+                if (_ is Window w)
+                    w.Close();
+                if (result.PlayerWon)
+                    new NavigationService(_navStore, () => new HomeVM(_navStore)).Navigate();
+                else
+                    new NavigationService(_navStore, () => new MainMenuVM(_navStore)).Navigate();
+            });
+        }
+
+        public string ResultText => _result.PlayerWon ? "Вітаємо з перемогою!" : "Ви програли...";
+        public int UnitsKilled => _result.UnitsKilled;
+        public int UnitsLost => _result.UnitsLost;
+        public int Turns => _result.Turns;
+        public int MoneyEarned => _result.MoneyEarned;
+
+        public RelayCommand ReturnCommand { get; }
+    }
+}
